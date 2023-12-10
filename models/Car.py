dc7ae281f762fd13ac7c7621f9e766eb628cf23a
CDavidSV <88672259+CDavidSV@users.noreply.github.com>
2023-12-10 01:14:55 -0600

    Update Car.py

--- models/Car.py
+++ models/Car.py
@@ -14,7 +14,8 @@ class Car(mesa.Agent):
         self.speed = speed
         self.curr_speed = speed
         self.reaction_time = reaction_time
-        self.curr_iter = 0
+        self.curr_iter_stopped = 0
+        self.curr_iter_change_lane = 0
 
         self.direction_deltas = {
             "north": (0, 1),
@@ -31,12 +32,12 @@ class Car(mesa.Agent):
         """Mueve el carro en la direccion en la que esta mirando"""
         # Si el carro esta detenido
         if not self.is_moving:
-            self.curr_iter += 1
+            self.curr_iter_stopped += 1
 
             # Revisamos si en la iteracion actual el carro debe moverse (en base a su tiempo de reaccion)
-            if self.curr_iter >= self.reaction_time:
+            if self.curr_iter_stopped >= self.reaction_time:
                 self.is_moving = True
-                self.curr_iter = 0
+                self.curr_iter_stopped = 0
             else: # Si no debe moverse, retornamos
                 return
 
@@ -67,6 +68,7 @@ class Car(mesa.Agent):
             elif self.model.is_intersection(forward_pos): # Si hay una interseccion en frente, revisar el semaforo
                 traffic_light = self.model.get_traffic_light(forward_pos)
                 if traffic_light.get_state(direction) == "red": # Si el semaforo esta en rojo, no avanzar
+                    new_pos = self.change_lane(new_pos)
                     break
                 else:
                     new_pos = forward_pos # Si el semaforo esta en verde, avanzar
@@ -80,7 +82,6 @@ class Car(mesa.Agent):
         # Si la nueva posicion es la misma que la actual, el carro esta detenido
         if new_pos == self.pos:
             self.is_moving = False
-            return
 
         # Si el carro se sale del grid, removerlo
         if self.model.grid.out_of_bounds(new_pos):
@@ -97,23 +98,23 @@ class Car(mesa.Agent):
         if not (0 <= xPos <= 3):
             return new_pos
         
-        self.curr_iter += 1
-        if self.curr_iter < self.reaction_time:
+        self.curr_iter_change_lane += 1
+        if self.curr_iter_change_lane < self.reaction_time:
             return new_pos 
 
         # Obtener los vecinos del carro
-        neighbors = self.model.grid.get_neighbors(self.pos, moore=True, include_center=False, radius=3)
+        neighbors = self.model.grid.get_neighbors(self.pos, moore=True, include_center=False, radius=10)
         
         # Determinar cual es la distancia al carro mas cercano en cada carril
-        left_lane_dist = self.nearest_car_distance(neighbors, xPos - 1)
-        right_lane_dist = self.nearest_car_distance(neighbors, xPos + 1)
+        left_lane_dist = self.nearest_car_distance(neighbors, xPos - 1) if xPos > self.model.first_lane else 0
+        right_lane_dist = self.nearest_car_distance(neighbors, xPos + 1) if xPos < 3 else 0
 
         # Decidir a que carril moverse
-        if left_lane_dist >= right_lane_dist and not self.model.has_car((xPos - 1, yPos)) and xPos > self.model.first_lane:
-            self.curr_iter = 0
+        if left_lane_dist >= right_lane_dist and self.has_enough_space(xPos, yPos, -1) and xPos > self.model.first_lane:
+            self.curr_iter_change_lane = 0
             return (xPos - 1, yPos) # Moverse al canal izquierdo
-        elif right_lane_dist >= left_lane_dist and not self.model.has_car((xPos + 1, yPos)) and xPos < 3:
-            self.curr_iter = 0
+        elif right_lane_dist >= left_lane_dist and self.has_enough_space(xPos, yPos, 1) and xPos < 3:
+            self.curr_iter_change_lane = 0
             return (xPos + 1, yPos) # Moverse al canal derecho
 
         return new_pos
@@ -122,9 +123,30 @@ class Car(mesa.Agent):
         """Encuentra la distancia al carro mas cercano en el carril dado"""
         lane_cars = [car for car in neighbors if car.pos[0] == lane_x]
         if not lane_cars:
-            return float('inf')  # No hay carros en el carril
+            return float('inf') # No hay carros en el carril
         distances = [abs(car.pos[1] - self.pos[1]) for car in lane_cars]
         return min(distances)
+    
+    def has_enough_space(self, xPos, yPos, delta):
+        """Revisa si el carro tiene suficiente espacio para cambiar de carril"""
+        # El valor de delta representa el carril al que queremos cambiar
+        # Aqui el carro se encuentra en el centro, Si el delta es -1, significa que queremos que cambie al carril izquierdo (Los cuadros vacios son donde no hay carros)
+        # En este ejemplo, el carro no se puede mover a la izquierda porque no hay suficiente espacio
+        # □ ■ □
+        # □ ■ □ <- Carro actual
+        # ■ □ □
+        # ■ □ □
+
+        if self.model.has_car((xPos + delta, yPos)):
+            return False
+        if self.model.has_car((xPos + delta, yPos - 1)):
+            return False
+        if self.model.has_car((xPos + delta, yPos - 2)):
+            return False
+        if self.model.has_car((xPos + delta, yPos + 1)):
+            return False
+        
+        return True
 
     def step(self):
         self.move()
